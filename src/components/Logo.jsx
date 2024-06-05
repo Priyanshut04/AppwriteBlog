28e53d4d57502df7facb355f969a4e7cb8b1f234
Priyanshut04 <priyanshutiwari.p403@gmail.com>
2024-06-05 10:54:34 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width='100px'}) {
+  return (
+    <div>
+      <img src="public\png-transparent-blogger-computer-icons-logo-blogger-logo-icon-svg-miscellaneous-text-trademark-thumbnail.png" alt='logo' style={{width: width}} />
+    </div>
+  )
+}
+
+export default Logo
