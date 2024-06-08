394a3fec9b195f4fdaa7326de611e359222a3dc0
Priyanshut04 <priyanshutiwari.p403@gmail.com>
2024-06-08 16:03:58 +0530

    rectify logo address

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Logo({width='100px'}) {
   return (
     <div>
-      <img src="src\components\logo.png" alt='logo' style={{width: width}} />
+      <img src="https://freepngimg.com/download/icon/app/919-blog.png" alt='logo' style={{width: width}} />
     </div>
   )
 }
